d4e3902565892be98936d3414e5038bd86691127
HassanFinitic <a.hassan@finitic.com>
2025-09-08 16:35:16 +0300

    handle the translation

--- src/components/Layout/Header/Products/Products.tsx
+++ src/components/Layout/Header/Products/Products.tsx
@@ -1,35 +1,37 @@
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export default function Products() {
-    
+    const { t } = useTranslation();
+
     const products = [
         {
-            name: "Client",
+            name: t("header.projects.client"),
             active: true,
             color: "rgba(125, 242, 125, 1)",
             fontColor: "rgba(14, 171, 96, 1)",
             activeColor: "rgba(229, 245, 237, 1)",
         },
         {
-            name: "IB",
+            name: t("header.projects.ib"),
             active: false,
             color: "rgba(255, 128, 129, 1)",
             activeColor: "rgba(254, 244, 242, 1)",
         },
         {
-            name: "Investor",
+            name: t("header.projects.investor"),
             active: false,
             color: "rgba(167, 105, 254, 1)",
             activeColor: "rgba(244, 244, 244, 1)",
         },
         {
-            name: "Provider",
+            name: t("header.projects.provider"),
             active: false,
             color: "rgba(147, 207, 255, 1)",
             activeColor: "rgba(244, 244, 244, 1)",
         },
         {
-            name: "Gold",
+            name: t("header.projects.gold"),
             active: false,
             color: "rgba(255, 243, 140, 1)",
             activeColor: "rgba(253, 248, 220, 1)",
@@ -59,7 +61,7 @@ export default function Products() {
                     {product.name}
                 </Typography>
                 <Typography fontSize={8} fontWeight={700}>
-                    Portal
+                    {t("header.projects.portal")}
                 </Typography>
             </Box>
         ))}
