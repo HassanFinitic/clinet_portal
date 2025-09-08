7eb189b5c2fcea0b998f3ece1aa83c1587da37f6
HassanFinitic <a.hassan@finitic.com>
2025-09-08 16:35:57 +0300

    create the dashboard page

---
+++ src/pages/Dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import ClientJourney from "../../components/Pages/Dashboard/ClientJourney/ClientJourney";
+
+export default function Dashboard() {
+  return (
+    <Box>
+        <ClientJourney />
+    </Box>
+  )
+}
