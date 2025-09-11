2468a9358975f456d663a5379d96488709a6bef7
HassanFinitic <a.hassan@finitic.com>
2025-09-11 12:18:31 +0300

    export all button in one componet

---
+++ src/components/Shared/Buttons/index.tsx
@@ -0,0 +1,7 @@
+export { default as DropDownButton } from "./DropDownButton/DropDownButton";
+export { default as GreenButton } from "./GreenButton/GreenButton";
+export { default as RedButton } from "./RedButton/RedButton";
+export { default as YellowButton } from "./YellowButton/YellowButton";
+export { default as PrimaryButton } from "./PrimaryButton/PrimaryButton";
+export { default as SecondaryButton } from "./SecondaryButton/SecondaryButton";
+export { default as CustomeButton } from "./CustomeButton/CustomeButton";
