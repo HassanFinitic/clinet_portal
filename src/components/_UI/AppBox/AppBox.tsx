1ea1b71df63e43d0b9ee66fcc7e123c005a658e2
HassanFinitic <a.hassan@finitic.com>
2025-09-12 13:53:28 +0300

    refactor the AppBox component to can accept the sx as props

--- src/components/_UI/AppBox/AppBox.tsx
+++ src/components/_UI/AppBox/AppBox.tsx
@@ -1,17 +1,19 @@
 import { Box } from '@mui/material'
+import type { SxProps } from '@mui/material';
 
-export default function AppBox({ children }: { children: React.ReactNode }) {
+export default function AppBox({ children, sx }: { children: React.ReactNode; sx?: SxProps }) {
   return (
     <Box
-    sx={{
-        padding: "16px 24px",
-        backgroundColor: "background.paper",
-        borderRadius: "44px",
-        border: "1px solid",
-        borderColor: "divider",
-        width: "100%",
-        marginBottom: "16px",
-    }}
+      sx={{
+          padding: "16px 24px",
+          backgroundColor: "background.paper",
+          borderRadius: "44px",
+          border: "1px solid",
+          borderColor: "divider",
+          width: "100%",
+          marginBottom: "16px",
+          ...sx
+      }}
     >
         {children}
     </Box>
