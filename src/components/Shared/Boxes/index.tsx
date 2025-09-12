065ba7dcf9b151c6d4382ad1577340e15bbf8632
HassanFinitic <a.hassan@finitic.com>
2025-09-12 13:55:09 +0300

    export all Boxes in one componet

---
+++ src/components/Shared/Boxes/index.tsx
@@ -0,0 +1,3 @@
+export { default as BoxRounded24 } from "./BoxRounded24/BoxRounded24";
+export { default as BoxRounded44 } from "./BoxRounded44/BoxRounded44";
+export { default as CustomeBox } from "./CustomeBox/CustomeBox";
